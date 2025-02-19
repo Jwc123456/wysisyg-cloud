8359f9585b6402d91475a05ca5ba35a2ca140802
wwcc <1351786511@qq.com>
2025-02-19 21:40:59 +0800

    代码生成器完善

--- wysiwyg-server/wysiwyg-admin/src/main/java/com/wysiwyg/admin/controller/system/HomeController.java
+++ wysiwyg-server/wysiwyg-admin/src/main/java/com/wysiwyg/admin/controller/system/HomeController.java
@@ -1,8 +1,26 @@
-package com.wysiwyg.admin.controller.system;
-
-/**
- * @author wwcc
- * @date 2025/02/12 18:53:53
- */
-public class HomeController {
-}
+//package com.wysiwyg.admin.controller.system;
+//
+//import com.wysiwyg.admin.model.po.AdminMenuPO;
+//import com.wysiwyg.admin.service.AdminMenuService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.List;
+//
+///**
+// * @author wwcc
+// * @date 2025/02/12 18:53:53
+// */
+//@RestController
+//@RequestMapping("/home")
+//public class HomeController {
+//
+//    @Autowired
+//    private AdminMenuService adminMenuService;
+//
+//    public String test(){
+//
+//        return null;
+//    }
+//}
